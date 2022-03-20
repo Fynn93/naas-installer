76568c517f065859f61aa61ae11238307477c09d
Fynn Gutic <66220444+Fynn93@users.noreply.github.com>
2022-03-20 18:15:26 +0100

    Upload Files

---
+++ NAAS_Installer/Form1.cs
@@ -0,0 +1,56 @@
+using System.Diagnostics;
+using AutoUpdaterDotNET;
+
+namespace NAAS_Installer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string installdir = AppDomain.CurrentDomain.BaseDirectory;
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.Text = $"Change the Install Directory.\nCurrent: {installdir}";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult fbd = folderBrowserDialog1.ShowDialog();
+            if(fbd == DialogResult.OK && folderBrowserDialog1.SelectedPath != null)
+            {
+                installdir = folderBrowserDialog1.SelectedPath;
+                richTextBox1.Text = $"Change the Install Directory.\nCurrent: {installdir}";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Process.Start("https://wiki.fynn93.tech/New%20Adventure%20All%20Stars/Installation", );
+            Process.Start(new ProcessStartInfo { FileName = "https://wiki.fynn93.tech/index.php?title=New_Adventure_All_Stars/Installation", UseShellExecute = true });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AutoUpdater.InstalledVersion = new Version("2.0.0.0");
+            AutoUpdater.RunUpdateAsAdmin = false;
+            AutoUpdater.DownloadPath = Application.StartupPath + "update";
+            AutoUpdater.ReportErrors = true;
+            AutoUpdater.Mandatory = true;
+            AutoUpdater.UpdateMode = Mode.Forced;
+            var currentDirectory = new DirectoryInfo(installdir);
+            if (currentDirectory.Parent != null)
+            {
+                AutoUpdater.InstallationPath = currentDirectory.FullName;
+            }
+            AutoUpdater.Start("https://api.fynn93.tech/naas/update.xml");
+        }
+    }
+}
